0d17499ba49c040a0bf862e54fcb9bb2aadeb9e7
IzharMohammed <izharmohammed21@gmail.com>
2025-09-17 01:07:35 +0530

    removed thedeals

--- constants/projects.ts
+++ constants/projects.ts
@@ -19,23 +19,23 @@ export const projects = [
             "SaaS",
         ],
     },
-    {
-        title: "TheDeals",
-        description:
-            "A SaaS platform with role-based access control featuring admin and customer roles. Admins can manage products, banners, and store configurations while customers enjoy a seamless shopping experience.",
-        image: "/assets/thedeals.png",
-        liveUrl: "https://www.thedeals.site/",
-        videoUrl: null,
-        tags: [
-            "T3 Stack",
-            "Next js",
-            "TRPC",
-            "TypeScript",
-            "Better-auth",
-            "Role-based Auth",
-            "SaaS",
-        ],
-    },
+    // {
+    //     title: "TheDeals",
+    //     description:
+    //         "A SaaS platform with role-based access control featuring admin and customer roles. Admins can manage products, banners, and store configurations while customers enjoy a seamless shopping experience.",
+    //     image: "/assets/thedeals.png",
+    //     liveUrl: "https://www.thedeals.site/",
+    //     videoUrl: null,
+    //     tags: [
+    //         "T3 Stack",
+    //         "Next js",
+    //         "TRPC",
+    //         "TypeScript",
+    //         "Better-auth",
+    //         "Role-based Auth",
+    //         "SaaS",
+    //     ],
+    // },
     {
         title: "Medium Clone",
         description:
